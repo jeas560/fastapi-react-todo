5253da079c24ee562c0da59e7230a99f950fb29b
jeas560 <jeas560@gmail.com>
2023-09-26 15:48:07 -0300

    separando as operações de base de dados da main

---
+++ backend/backend/database.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine, Column, Integer, String, Time
+from sqlalchemy.ext.declarative import declarative_base
+
+# Criando uma instancia de engine sqlite
+engine = create_engine("sqlite:///todo.db")
+
+# Criando uma metaclasse Declarative
+Base = declarative_base()
+
+
+# Definindo a classe ToDo, herdeira da classe Base
+class ToDo(Base):
+    __tablename__ = "todos"
+    id = Column(Integer, primary_key=True)
+    task = Column(String(256))
+    suggested_time = Column(Time, default=None)
--- backend/backend/main.py
+++ backend/backend/main.py
@@ -1,21 +1,5 @@
 from fastapi import FastAPI, status
-from sqlalchemy import create_engine, Column, Integer, String, Time
-from sqlalchemy.ext.declarative import declarative_base
-
-# Criando uma instancia de engine sqlite
-engine = create_engine("sqlite:///todoo.db")
-
-# Criando uma metaclasse Declarative
-Base = declarative_base()
-
-
-# Definindo a classe ToDo, herdeira da classe Base
-class ToDo(Base):
-    __tablename__ = "todos"
-    id = Column(Integer, primary_key=True)
-    task = Column(String(256))
-    suggested_time = Column(Time)
-
+from database import Base, engine
 
 # Criando a base de dados
 Base.metadata.create_all(engine)
