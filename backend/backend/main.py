3698654d2f507e407eeb817fcf2971bdb527fb25
jeas560 <jeas560@gmail.com>
2023-11-22 20:41:27 -0300

    Declaring Response Models

--- backend/backend/main.py
+++ backend/backend/main.py
@@ -3,6 +3,7 @@ from database import Base, engine
 from sqlalchemy.orm import Session
 import models
 import schemas
+from typing import List
 
 
 # Criando a base de dados
@@ -17,7 +18,7 @@ def root():
     return "App -- lista de tarefas"
 
 
-@app.post("/todo", status_code=status.HTTP_201_CREATED)
+@app.post("/todo", response_model=schemas.ToDoWithId, status_code=status.HTTP_201_CREATED)
 def create_todo(todo: schemas.ToDo):
     # Criando uma nova sessão da base de dados
     session = Session(bind=engine, expire_on_commit=False)
@@ -28,17 +29,17 @@ def create_todo(todo: schemas.ToDo):
     # Adicionando a instância e comitando
     session.add(tododb)
     session.commit()
-
+    
     # Pegando a id dada ao objeto pela base de dados
-    id = tododb.id
+    session.refresh(tododb)
 
     # Encerrando a sessão
     session.close()
 
-    return f"criado um item na lista com a id {id}"
+    return tododb
 
 
-@app.get("/todo/{id}")
+@app.get("/todo/{id}", response_model=schemas.ToDoWithId)
 def read_todo(id: int):
     # Criando uma nova sessão da base de dados
     session = Session(bind=engine, expire_on_commit=False)
@@ -52,23 +53,23 @@ def read_todo(id: int):
     # Verificando se o item existe ao ser procurado pelo id
     # Se não, levanta uma exceção e retorna 404: não encontrado
     if not todo:
-        raise HTTPException(status_code=404, detail=f"item com o {id} não encontrado")
+        raise HTTPException(status_code=404, detail=f"item com o id: {id}, não encontrado")
 
     return todo
 
 
-@app.put("/todo/{id}")
-def update_todo(id: int, todo_mod: schemas.ToDo):
+@app.put("/todo/{id}", response_model=schemas.ToDoWithId)
+def update_todo(id: int, todo: schemas.ToDo):
     # Criando uma nova sessão da base de dados
     session = Session(bind=engine, expire_on_commit=False)
 
     # Pegando um item pelo id na base de dados
-    todo = session.query(ToDo).get(id)
+    todo_mod = session.query(models.ToDo).get(id)
 
     # Atualizar um item com as novas informações (caso for encontrado)
-    if todo:
-        todo.task = todo_mod.task
-        todo.suggested_time = todo_mod.suggested_time
+    if todo_mod:
+        todo_mod.task = todo.task
+        todo_mod.suggested_time = todo.suggested_time
         session.commit()
 
     # Encerrando a sessão
@@ -76,10 +77,10 @@ def update_todo(id: int, todo_mod: schemas.ToDo):
 
     # Verificando se o item existe ao ser procurado pelo id
     # Se não, levanta uma exceção e retorna 404: não encontrado
-    if not todo:
-        raise HTTPException(status_code=404, detail=f"item com o {id} não encontrado")
+    if not todo_mod:
+        raise HTTPException(status_code=404, detail=f"item com o id: {id}, não encontrado")
 
-    return todo
+    return todo_mod
 
 
 @app.delete("/todo/{id}", status_code=status.HTTP_204_NO_CONTENT)
@@ -97,12 +98,12 @@ def delete_todo(id: int):
         session.commit()
         session.close()
     else:
-        raise HTTPException(status_code=404, detail=f"item com o {id} não encontrado")
+        raise HTTPException(status_code=404, detail=f"item com o id: {id}, não encontrado")
 
     return None
 
 
-@app.get("/todo")
+@app.get("/todo", response_model = List[schemas.ToDoWithId])
 def read_todo_list():
     # Criando uma nova sessão da base de dados
     session = Session(bind=engine, expire_on_commit=False)
--- backend/backend/schemas.py
+++ backend/backend/schemas.py
@@ -1,8 +1,18 @@
 from pydantic import BaseModel
 from datetime import time
-from typing import Union
+from typing import Union, Optional
+
 
 # Criando a classe ToDoRequest, herdeira da classe BaseModel (Pydantic)
 class ToDo(BaseModel):
     task: str
     suggested_time: Union[time, None] = None
+
+
+class ToDoWithId(BaseModel):
+    id: int
+    task: str
+    suggested_time: Union[time, None] = None
+
+    class Config:
+        from_attributes = True
